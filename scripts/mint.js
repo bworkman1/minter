1e35461229b5d1183f10a45dc8f10878991a13b9
Brian <brianworkman43055@gmail.com>
2022-04-10 14:01:49 -0400

    Initial setup

--- scripts/mint.js
+++ scripts/mint.js
@@ -1,5 +1,5 @@
 const Web3 = require('web3')
-const web3 = new Web3('http://127.0.0.1:7545')
+const web3 = new Web3('http://127.0.0.1:8545')
 
 const nftBuild = require('../build/contracts/NFT.json')
 const nftContract = new web3.eth.Contract(nftBuild.abi, nftBuild.networks[5777].address)
